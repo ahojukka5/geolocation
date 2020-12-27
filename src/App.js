6d0a3266572a608bebde3c5b2b53d4a45635ccf6
Jukka Aho <ahojukka5@gmail.com>
2020-12-27 16:43:02 +0700

    First version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+  const [position, setPosition] = useState();
+  const [message, setMessage] = useState('initializing app');
+
+  const success = (pos) => {
+    var crd = pos.coords;
+    setPosition(crd);
+  };
+
+  const errors = (err) => {
+    setMessage(`ERROR(${err.code}): ${err.message}`);
+  };
+
+  useEffect(() => {
+    const getPosition = async () => {
+      if (!navigator.geolocation) {
+        setMessage('navigator.geolocation not found');
+        return;
+      }
+      const result = await navigator.permissions.query({ name: 'geolocation' });
+      setMessage(`result.state = ${result.state}`);
+      if (result.state === 'granted') {
+        navigator.geolocation.getCurrentPosition(success);
+      } else if (result.state === 'prompt') {
+        const options = {
+          enableHighAccuracy: true,
+          timeout: 5000,
+          maximumAge: 0,
+        };
+        navigator.geolocation.getCurrentPosition(success, errors, options);
+      }
+    };
+    getPosition();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>GeoLocation</h2>
+      <p>message: {message}</p>
+      {position && (
+        <div>
+          <div>Your current position is:</div>
+          <div>Latitude: {position.latitude}</div>
+          <div>Longitude: {position.longitude}</div>
+          <div>Accuracy: {position.accuracy} meters</div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
